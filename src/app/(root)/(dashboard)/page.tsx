a8156c952ff2023b5352ac054cf2419e1f41aa5f
Geraldo Luiz <geraldobl58@gmail.com>
2024-07-25 10:57:40 -0300

    feat: Add layout and root page

--- src/app/(root)/(dashboard)/page.tsx
+++ src/app/(root)/(dashboard)/page.tsx
@@ -1,7 +1,13 @@
+import { Container } from '@/components/container'
+import { Header } from '@/components/header'
+
 const DashboardPage = () => {
   return (
     <div>
-      <div>DashboardPage</div>
+      <Header title="Dashboard" />
+      <Container>
+        <div>Dashboardpage</div>
+      </Container>
     </div>
   )
 }
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,16 @@
+import { Sidebar } from '@/components/sidebar'
+
 interface RootLayoutProps {
   children: React.ReactNode
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => {
-  return <div>{children}</div>
+  return (
+    <div className="flex w-full h-full">
+      <Sidebar />
+      <main className="flex-1">{children}</main>
+    </div>
+  )
 }
 
 export default RootLayout
